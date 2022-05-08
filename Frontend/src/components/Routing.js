ca275398f5903d06f7469590b1168af696755874
navod99 <navx99@gmail.com>
2022-05-08 12:53:26 +0530

    created login signup form

--- Frontend/src/components/Routing.js
+++ Frontend/src/components/Routing.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Home from './Home';
 import Login from './Login';
+import Signup from './Signup';
 
 const Routing = () => {
 
@@ -14,6 +15,7 @@ const Routing = () => {
             <Routes>
                 <Route path='/' exact element= {<Home/>}/>
                 <Route path='/login' exact element= {<Login/>}/>
+                <Route path='/signup' exact element= {<Signup/>}/>
               
 
             </Routes>
